8c931e63fac095ceebbfb5736732e94a572d4840
Thierry Bela <bnanga@gmail.com>
2012-12-03 02:55:33 +0100

    adding validator and data api

--- Source/InPlaceEditor.Delegation.js
+++ Source/InPlaceEditor.Delegation.js
@@ -7,7 +7,7 @@ description: inplace editor.
 copyright: Copyright (c) 2010 Thierry Bela
 authors: [Thierry Bela]
 
-requires: 
+requires:
   InPlaceEditor
 provides: [InPlaceEditor.Delegation]
 ...
@@ -18,7 +18,7 @@ provides: [InPlaceEditor.Delegation]
 "use strict";
 
 	function wrapper(InPlaceEditor) {
-		
+
 		InPlaceEditor.Delegation = new Class({
 
 			/*
@@ -29,34 +29,60 @@ provides: [InPlaceEditor.Delegation]
 			*/
 
 			Extends: InPlaceEditor,
-			
-			getEvents: function () {
+
+			getEvents: function (el) {
 
 				var self = this,
-					options = this.options,
+					options = Object.append({}, this.options, el && el.data()),
 					property = 'backgroundColor',
 					relay = ':relay(' + options.relay + ')';
 
 				return [
 							function(e, el) { el.tween(property, options.toColor) },
-							function(e, el) { 
-								
-								el.tween(property, options.fColor).get('tween').chain(function () { el.setStyle(property, el.retrieve('eip-color')) }) 
+							function(e, el) {
+
+								el.tween(property, options.fColor).get('tween').chain(function () { el.setStyle(property, el.retrieve('eip-color')) })
 							},
 							function(e, el) {
 
 								e.stop();
-								self.build(el.store('eip-edit', 1))
+								self.build(el, options)
 							}
 
 						].associate(['mouseenter' + relay, 'mouseleave' + relay, 'click' + relay])
 			}
 		})
-		
+
+		 /* InPlaceEditor DATA-API
+		  * ============== */
+
+		 var instance = new InPlaceEditor.Delegation(), key = 'data-inplace-edit';
+
+		window.addEvent('domready', function () {
+			
+			document.body.addEvent('click:relay([data-toggle=inplace-edit-delegate])', function (e, el) {
+
+				// already in use
+				if(e.target.retrieve('data-inplace-edition')) return;
+
+				var options = Object.append({}, el.data()),
+					target = options.relay && (e.target.match(options.relay) && e.target || e.target.getParent(options.relay));
+				
+				// if(!options.relay && window.console && console.log) console.log('missing selector: relay')
+				if(target) {
+
+					e.preventDefault();
+
+					if(!el.retrieve(key)) instance.attach(el.store(key, instance));
+					instance.build(target, options)
+				}
+			})
+		});
+
 		return InPlaceEditor
 	}
-	
+
 	if(typeof define == 'function' && define.amd) define(['./InPlaceEditor'], wrapper);
-	else  wrapper(window.InPlaceEditor)
-	
+	else wrapper(window.InPlaceEditor)
+
 }(this);
